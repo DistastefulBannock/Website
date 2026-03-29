8576319ac7f4efb3f0ec76d8de33ea6759c633e0
DistastefulBannock <DistastefulBannock@gmail.com>
2026-03-29 19:46:35 -0400

    Better error pages.

--- src/main/java/me/bannock/website/controllers/core/CoreErrorController.java
+++ src/main/java/me/bannock/website/controllers/core/CoreErrorController.java
@@ -1,8 +1,11 @@
 package me.bannock.website.controllers.core;
 
 import brave.Tracer;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +17,18 @@ public class CoreErrorController implements ErrorController {
     private Tracer tracer;
 
     @GetMapping("/error")
-    public String handleError(Model model){
+    public String handleError(HttpServletRequest request, Model model){
+        String userFriendlyMessage = "Something went wrong while processing your request";
+        Object exceptionObj = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        if (exceptionObj instanceof Exception) {
+            Exception exception = (Exception) exceptionObj;
+            if (exception instanceof InsufficientAuthenticationException){
+                userFriendlyMessage = exception.getMessage();
+            }
+        }
+
         model.addAttribute("tId", tracer.currentSpan().context().traceIdString());
-        model.addAttribute("userFriendlyMessage",
-                "Something went wrong while processing your request");
+        model.addAttribute("userFriendlyMessage", userFriendlyMessage);
         return "core/error";
     }
 
